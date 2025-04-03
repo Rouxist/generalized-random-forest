df859fc9a86ebd0d364775fe359df4e165ed3b9d
Rouxist <name.of.squaner@gmail.com>
2025-04-03 20:40:19 +0900

    feat: remove redundant X, y aggregation process in predict function

--- grf_cython/_generalized_random_forest.py
+++ grf_cython/_generalized_random_forest.py
@@ -33,6 +33,8 @@ class GRF:
         self.subsample_random_state_seed = 0
 
     def fit(self, X, y) -> None:
+        self.X = X.copy()
+        self.y = y.copy()
         # Subsample generation
         self.subsample_random_state_seed = self.random_state.randint(MAX_INT)
         subsample_random_state = np.random.RandomState(self.subsample_random_state_seed)
@@ -81,7 +83,7 @@ class GRF:
             for slice, tree in zip(slice_indices, trees))
 
         self.estimators_.extend(trees_fitted)
-
+    
     def predict(self, X)->np.ndarray:
         val_X_list = [self.estimators_[i].X_parent[self.estimators_[i].indices_val,:]
                             for i in range(self.n_estimators)]
@@ -89,28 +91,24 @@ class GRF:
                             for i in range(self.n_estimators)]
         
         # Pool data from all trees
-        pooled_val_X = np.concatenate(val_X_list)
         pooled_val_y = np.concatenate(val_y_list)
-        if pooled_val_y.ndim == 1:
-            pooled_val_y = np.expand_dims(pooled_val_y, (-1))
-        pooled_data = np.concatenate([pooled_val_y, pooled_val_X], axis=1)
+        if self.y.ndim == 1:
+            self.y = np.expand_dims(self.y, (-1))
 
-        # Drop duplicates
-        aggr_val_data = np.unique(pooled_data, axis=0)
-        aggr_val_X = aggr_val_data[:,1:].copy()
-        aggr_val_y = aggr_val_data[:,0].copy()
-        n_samples_val = aggr_val_X.shape[0]
+        n_samples_val = self.X.shape[0]
+        X_all = self.X
+        y_all = self.y
 
-        # Pre-calculate indices of {val datapoints in each tree} in the aggregated dataset
-        indices_from_whole = [[np.where((aggr_val_X == dp).all(axis=1))[0].squeeze() for dp in val_X_list[tree_idx]] for tree_idx in range(self.n_estimators)]
+        # Pre-calculate indices of {val datapoints in each tree} in the original dataset
+        indices_from_whole = [[np.where((X_all == dp).all(axis=1))[0].squeeze() for dp in val_X_list[tree_idx]] for tree_idx in range(self.n_estimators)]
 
         # Moment condition setup        
         def sum_moment_condition(theta, alpha) -> float: # Eq (2) of Athey, S., Tibshirani, J., & Wager, S. (2019). Generalized random forests.
             # Depends on Regression equation
-            return np.sum(alpha.dot(aggr_val_y-theta))
+            return np.sum(alpha.dot(y_all-theta))
         
         # Initial value: median
-        theta_0 = np.median(aggr_val_y)
+        theta_0 = np.median(pooled_val_y) # y_all
         
         # Output initialization
         n_given_datapoints = X.shape[0]
@@ -140,12 +138,12 @@ class GRF:
             
             for tree_idx in range(self.n_estimators):
                 leaf_idx = leaf_indices[tree_idx]
-                neighbors = (leaf_matrices[tree_idx][leaf_idx] > 0).squeeze()
+                neighbors = (leaf_matrices[tree_idx][leaf_idx] > 0).squeeze() # Same as `neighbors = leaf_matrices[tree_idx][leaf_idx]`. But this boolean expression executes faster.
 
                 for i, neighbor in enumerate(neighbors):
                     if neighbor:
-                        alpha[indices_from_whole[tree_idx][i]] += 1/sum(neighbors)/self.n_estimators
-            
+                        alpha[indices_from_whole[tree_idx][i]] += 1/sum(neighbors)/self.n_estimators # If neighbor, weight increases. Otherwise, it adds 0.
+
             res = fsolve(sum_moment_condition, theta_0, alpha)
             predictions[dp_idx] = res[0]
 
